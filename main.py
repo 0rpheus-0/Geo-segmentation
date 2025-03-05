8d3e064dd3c504340285863c2b51646b36a4232a
0rpheus-0 <erofeevvictor25@gmail.com>
2025-03-05 11:04:46 +0300

    Add constants, dataset, visual functions

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch
 import random
 from segmentation_models_pytorch import utils
+from constants import X_TRAIN_DIR, Y_TRAIN_DIR
+from Dataset import Dataset
+import visual
 
 seed = 42
 torch.manual_seed(seed)
@@ -12,40 +15,8 @@ random.seed(seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
-DATSET_NAME = 'geo_dataset'
-
-X_TRAIN_DIR = f'{DATSET_NAME}/train'
-Y_TRAIN_DIR = f'{DATSET_NAME}/train_mask/defaultannot'
-
-X_VALID_DIR = f'{DATSET_NAME}/validation'
-Y_VALID_DIR = f'{DATSET_NAME}/validation_mask/defaultannot'
-
-X_TEST_DIR = f'{DATSET_NAME}/test'
-Y_TEST_DIR = f'{DATSET_NAME}/test_mask/defaultannot'
-
-LABEL_COLORS_FILE = f'{DATSET_NAME}/label_colors.txt'
-
-CLASSES = [
-    'City',
-    'Cloud',
-    'Field',
-    'Mountains',
-    'Sand',
-    'Trees',
-    'Water'
-]
-ENCODER = 'resnet18'
-ENCODER_WEIGHTS = 'imagenet'
-ACTIVATION = 'softmax2d' 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-EPOCHS = 70
-BATCH_SIZE = 32
-
-INIT_LR = 0.0005
-LR_DECREASE_STEP = 15
-LR_DECREASE_COEF = 2 
-
-INFER_WIDTH = 384
-INFER_HEIGHT = 384
-
 loss = utils.losses.DiceLoss()
+      
+dataset = Dataset(X_TRAIN_DIR, Y_TRAIN_DIR)
+image, mask = dataset[np.random.randint(len(dataset))]
+visual.visualize_multichennel_mask(image, mask)
