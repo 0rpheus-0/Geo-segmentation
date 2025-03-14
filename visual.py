4232d24602433c4ca185c4ab1cf4c72ace070a11
0rpheus-0 <erofeevvictor25@gmail.com>
2025-03-14 21:35:09 +0300

    add visual result

--- visual.py
+++ visual.py
@@ -2,40 +2,79 @@ import numpy as np
 import matplotlib.pyplot as plt
 from constants import CLASSES
 
-def visualize_mask(mc_mask: np.ndarray):
-    colors_imshow = {
-        'City' : np.array([170, 240, 209]),
-        'Cloud' : np.array([184, 61, 245]),
-        'Field' : np.array([242, 242, 13]),
-        'Mountains' : np.array([61, 61, 245]),
-        'Sand': np.array([241, 184, 22]),
-        'Trees': np.array([63, 220, 32]),
-        'Water': np.array([13, 160, 236])
-    }
+colors_imshow = {
+    "City": np.array([170, 240, 209]),
+    "Cloud": np.array([184, 61, 245]),
+    "Field": np.array([242, 242, 13]),
+    "Mountains": np.array([61, 61, 245]),
+    "Sand": np.array([241, 184, 22]),
+    "Trees": np.array([63, 220, 32]),
+    "Water": np.array([13, 160, 236]),
+}
 
-    sc_mask = np.zeros((mc_mask[0].shape[0], mc_mask[0].shape[1], 3), dtype=np.uint8)
+
+def visual_mask(mask: np.ndarray):
+    sc_mask = np.zeros((mask[0].shape[0], mask[0].shape[1], 3), dtype=np.uint8)
     square_ratios = {}
-
-    for i, singlechannel_mask in enumerate(mc_mask):
-
+    for i, singlechannel_mask in enumerate(mask):
         cls = CLASSES[i]
         singlechannel_mask = singlechannel_mask.squeeze()
-
         square_ratios[cls] = singlechannel_mask.sum() / singlechannel_mask.size
-        
-        sc_mask += np.multiply.outer(singlechannel_mask > 0, colors_imshow[cls]).astype(np.uint8)
-        
+        sc_mask += np.multiply.outer(singlechannel_mask > 0, colors_imshow[cls]).astype(
+            np.uint8
+        )
 
-    title = 'Square: ' + '\n'.join([f'{cls}: {square_ratios[cls]*100:.1f}%' for cls in CLASSES])
+    title = "Square: " + "\n".join(
+        [f"{cls}: {square_ratios[cls] * 100:.1f}%" for cls in CLASSES]
+    )
     return sc_mask, title
 
-def visualize_multichennel_mask(img: np.ndarray, multichennel_mask: np.ndarray):
+
+def visualize_multichennel_mask(image: np.ndarray, multichennel_mask: np.ndarray):
     _, axes = plt.subplots(1, 2, figsize=(10, 5))
-    img = img.transpose(2, 0, 1)
-    axes[0].imshow(img[0], cmap="grey")
-    multichennel_mask = multichennel_mask.transpose(2, 0, 1)
-    mask_to_show, title = visualize_mask(multichennel_mask)
+    axes[0].imshow(image[0], cmap="grey")
+    mask_to_show, title = visual_mask(multichennel_mask)
     axes[1].imshow(mask_to_show)
     axes[1].set_title(title)
     plt.tight_layout()
-    plt.show()  
+    plt.show()
+
+
+def color_mask(mask: np.ndarray):
+    mask = mask.squeeze()
+    colored_mask = np.zeros((*mask.shape, 3), dtype=np.uint8)
+    square_ratios = {}
+    for cls_code, cls in enumerate(CLASSES):
+        cls_mask = mask == cls_code
+        square_ratios[cls] = cls_mask.sum() / cls_mask.size
+        colored_mask += np.multiply.outer(cls_mask, colors_imshow[cls]).astype(np.uint8)
+
+    return colored_mask, square_ratios
+
+
+def reverse_normalize(img, mean, std):
+    img = img * np.array(std) + np.array(mean)
+    return img
+
+
+def visualize_result(img: np.ndarray, mask_gt: np.ndarray, mask_pred: np.ndarray):
+    _, axes = plt.subplots(1, 3, figsize=(10, 5))
+
+    axes[0].imshow(img[0], cmap="grey")
+
+    mask_gt, square_ratios = color_mask(mask_gt)
+    title = "Square:\n" + "\n".join(
+        [f"{cls}: {square_ratios[cls] * 100:.1f}%" for cls in CLASSES]
+    )
+    axes[1].imshow(mask_gt, cmap="twilight")
+    axes[1].set_title("GT мask\n" + title)
+
+    mask_pred, square_ratios = color_mask(mask_pred)
+    title = "Square:\n" + "\n".join(
+        [f"{cls}: {square_ratios[cls] * 100:.1f}%" for cls in CLASSES]
+    )
+    axes[2].imshow(mask_pred, cmap="twilight")
+    axes[2].set_title("PRED мask\n" + title)
+
+    plt.tight_layout()
+    plt.show()
